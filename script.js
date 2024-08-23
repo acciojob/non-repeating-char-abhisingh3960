15b3de91054f9864e55b7917ee62697cdd0b72aa
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-23 11:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let obj ={};
 function firstNonRepeatedChar(str) {
- // Write your code here
-	for(let ch of str){
-		if(obj[ch]){
-			obj[ch]++;
-		}
-		else{
-			obj[ch] =1;
-		}
-	}
-	  for (let ch of str) {
+    let charCount = {};
+
+    for (let ch of str) {
+        if (charCount[ch]) {
+            charCount[ch]++;
+        } else {
+            charCount[ch] = 1;
+        }
+    }
+
+    for (let ch of str) {
         if (charCount[ch] === 1) {
             return ch;
         }
     }
-	return null;
+
+    return null;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
